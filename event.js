4c6c5d6ef8ce788f753f556d379d96d0381b8d8c
abusayadebu <abusayad743@gmail.com>
2026-02-15 17:10:03 +0600

    onclick and addEvenetListener practiced

--- event.js
+++ event.js
@@ -1,1 +1,15 @@
-console.log("event js connected");
+// console.log("event js connected");
+
+function makeYellow(){
+    document.body.style.backgroundColor = "yellow"
+}
+
+const greenButton = document.getElementById("make-green")
+ greenButton.onclick = function makeGreen(){
+    document.body.style.backgroundColor = "green"
+}
+
+// addEventlistener
+const purpleBtn = document.getElementById("purple-btn").addEventListener('click', function(){
+    document.body.style.backgroundColor = "purple"
+})
